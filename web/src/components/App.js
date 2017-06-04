3ea3b4d40bdb031616f4cda8848e4b9a53134dd7
muhammadridho <ulunmuhammadridho@gmail.com>
2017-06-04 08:31:50 +0800

    adding semantic ui

---
+++ web/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/css/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
+
+export default App;
